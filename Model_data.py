b82597d546277b71a018c1d42409ffaa7772c296
akritia3 <akritia3@illinois.edu>
2023-12-03 18:43:06 -0600

    Added sample pdfs (frontend)

--- Model_data.py
+++ Model_data.py
@@ -1,10 +1,9 @@
-
 #import spacy
 import csv
 import pandas as pd
 from Word_tokenizer import *
 import random
-
+from spacy.training.example import Example
 import spacy
 
 resume_keys = [
@@ -48,13 +47,13 @@ resume_keys = [
 resume_keys = get_most_used_words('UpdatedResumeDataSet.csv')
 
 resume_keys = resume_keys[:150]
-# print(resume_keys)
+#print(resume_keys)
 
 resume_first_val = [check[0] for check in resume_keys]
 
 df = pd.read_csv('UpdatedResumeDataSet.csv')
 dataset = df['Resume']
-## print(dataset.iloc[0])
+#print(dataset.iloc[0])
 
 
 #dataset = dataset.str.lower()
@@ -75,10 +74,10 @@ def calculate_keyword_percentage(df, resume_keywords):
             if word in resume_keywords and word not in my_set:
                 total_amt+=1
                 my_set.add(word)
-        # # print(total_amt)
-        # # print(total_sum)
+        #print(total_amt)
+        #print(total_sum)
         percentage_array.append((float)(total_amt / total_sum) * 100)
-        ## print(percentage_array[0])
+        #print(percentage_array[0])
         #break
     
     return percentage_array
@@ -101,8 +100,9 @@ def calculate_single_percentage(words, resume_keywords):
 df_part_one = df.head(len(df)//2)
 df_part_two = df.tail(len(df)//2)
 
-percentage_array = calculate_keyword_percentage(df_part_one, resume_first_val)
-# print(percentage_array)
+percentage_array = calculate_keyword_percentage(df, resume_first_val)
+#print(percentage_array)
+
 
 
 def returnGoodBadResumes(percentage_array):
@@ -113,14 +113,14 @@ def returnGoodBadResumes(percentage_array):
     for percent in percentage_array:
         # get data only from the column required
         #required = resume['Resume']
-        ## print(percent)
+        #print(percent)
         if percent >= 25:
             good_resumes.append(dataset[index])
         else: bad_resumes.append(dataset[index])
         index +=1
 
-    # print("Length good:", len(good_resumes))
-    # print("Length bad:",len(bad_resumes))
+    #print("Length good:", len(good_resumes))
+    #print("Length bad:",len(bad_resumes))
 
     return good_resumes, bad_resumes
 
@@ -129,197 +129,77 @@ def returnGoodBadResumes(percentage_array):
 #nlp = spacy.load('en_core_web_sm')
 
 
+def addColumnGoodBad(percentage_array):
+    vals = []
+    for percent in percentage_array:
+        if percent >= 25:
+            vals.append("Good")
+        else: vals.append("Bad")
+    df["Good or Bad"] = vals
+addColumnGoodBad(percentage_array)
+#This method is through Chat GPT
+
+#this method taken from the internet and chat gpt
+def train_spacy() :
+    addColumnGoodBad(percentage_array)
+    # Load the English language model
+    nlp = spacy.load("en_core_web_sm")
+
+    new_df = df.iloc[:, -2:]
+
+    # Define the training configurations
+    config = {
+        'epochs': 10,
+        'batch_size': 8,
+        'learning_rate': 0.001,
+    }
+
+    # # Create training data in spaCy format
+    train_data = []
+    for text, label in zip(df['Resume'], df['Good or Bad']):
+        # Convert label to binary (1 for good, 0 for bad)
+        label_binary = 1 if label == 'good' else 0
+        train_data.append((text, {'cats': {'Good': label_binary, 'Bad': 1 - label_binary}}))
+
+    # # Initialize the text classification pipeline in spaCy
+    text_cat = nlp.add_pipe('textcat', config={'exclusive_classes': True, 'architecture': 'simple_cnn'})
+    text_cat.add_label('Good')
+    text_cat.add_label('Bad')
+
+    # # Train the model
+    random.shuffle(train_data)
+    for epoch in range(config['epochs']):
+        losses = {}
+        for text, annotations in train_data:
+            doc = nlp.make_doc(text)
+            example = Example.from_dict(doc, annotations)
+            nlp.update([example], drop=0.5, losses=losses)
+        print(f'Epoch {epoch + 1}, Loss: {losses["textcat"]:.3f}')
+
+    # # Save the trained model to a file
+    nlp.to_disk('Spacyload.txt')
+
+
 def assess_resume(resume_text):
+    # train_spacy()
+    # nlp = spacy.load('Spacyload.txt')
     nlp = spacy.load('en_core_web_sm')
     resume_analysis = nlp(resume_text)
-    # print(resume_analysis)
+    #print(resume_analysis)
     words = [token.text for token in resume_analysis]
     percent = calculate_single_percentage(words, resume_first_val)
     
-    # print(percent)
+    #print(percent)
     if percent >= 25:
         return ("Good resume", percent)
     else : return ("Bad resume", percent)
 
 
-for i in range(150):
-    second_element_first_row = df.iloc[i, 1]
-    # print(assess_resume(second_element_first_row))
-    #break
-# import csv
-# import pandas as pd
+def getMissingWords(resume_text, used_words):
+    vals = []
+    for word in resume_text:
+        if word not in used_words:
+            vals.append(word)
 
-# import spacy
-
-# resume_keys = [
-#     "Objective",
-#     "Summary",
-#     "Computer",
-#     "Data",
-#     "Profile",
-#     "Professional Summary",
-#     "Career Summary",
-#     "Qualifications",
-#     "Skills",
-#     "python",
-#     "january",
-#     "August",
-#     "industry",
-#     "technical",
-#     "exprience"
-#     "Areas of Expertise",
-#     "Key Skills",
-#     "Technical Skills",
-#     "Education",
-#     "Academic Background",
-#     "Professional Experience",
-#     "Work Experience",
-#     "javascript",
-#     "Employment History",
-#     "Relevant Experience",
-#     "Projects",
-#     "Certifications",
-#     "Awards and Honors",
-#     "Achievements",
-#     "Publications",
-#     "Languages",
-#     "Interests",
-#     "Volunteer Work",
-#     "References",
-# ]
-
-# resume_keywords = [value.lower() for value in resume_keys]
-# # data_temp = []
-# # with open('/Users/vikrampidaparthi/Documents/CS222_Project/group-project-team69/UpdatedResumeDataSet 3.csv', 'r', newline='') as csvfile:
-# #     reader = csv.DictReader(csvfile)
-# #     for row in reader:
-# #         #data_temp = [row[1] for row in reader]
-# #         # print(row)
-# #         break
-# # # print(data_temp)
-# # dataset = []
-# # dataset.append(row[1] in data_temp[1])
-# #dataset = [row[1] for row in data_temp[1:]]
-
-
-
-# df = pd.read_csv('/Users/vikrampidaparthi/Documents/CS222_Project/group-project-team69/UpdatedResumeDataSet 3.csv')
-# dataset = df['Resume']
-# ## print(dataset.iloc[0])
-
-
-# dataset = dataset.str.lower()
-
-
-
-
-
-
-# def calculate_keyword_percentage(df, resume_keywords):
-    
-#     percentage_array = []
-#     for index, row in df.iterrows():
-#         total_sum = len(resume_keywords)
-#         total_amt = 0
-#         words = row['Resume'].split()
-        
-#         for word in words:
-#             if word in resume_keywords:
-#                 total_amt+=1
-#         # print(total_amt)
-#         # print(total_sum)
-#         percentage_array.append((float)(total_amt / total_sum) * 100)
-#         # print(percentage_array[0])
-#         #break
-    
-#     return percentage_array
-    
-
-# percentage_array = calculate_keyword_percentage(df, resume_keywords)
-
-# index = 0
-# good_resumes = []
-# bad_resumes = []
-# for percent in percentage_array:
-#     # get data only from the column required
-#     #required = resume['Resume']
-#     ## print(percent)
-#     if percent >= 30:
-#         good_resumes.append(dataset[index])
-#     else: bad_resumes.append(dataset[index])
-#     index +=1
-
-# # print("Length good:", len(good_resumes))
-# # print("Length bad:",len(bad_resumes))
-
-# #check the database and do NLP:
-
-# # Load spaCy's English language model
-# nlp = spacy.load("en_core_web_sm")
-
-# # Define your set of keywords and their importance scores
-# keywords = {
-#     "Objective" : 0.1,
-#     "Summary": 0.2,
-#     "Computer": 0.1,
-#     "Data": 0.6,
-#     "Profile": 0.1,
-#     "Professional Summary": 0.1,
-#     "Career Summary": 0.1,
-#     "Qualifications": 1.5,
-#     "Skills": 3,
-#     "python": 2,
-#     "january": 0.1,
-#     "August": 0.1,
-#     "industry": 0.1,
-#     "technical": 0.1,
-#     "exprience": 5,
-#     "Areas of Expertise": 0.1,
-#     "Key Skills": 0.1,
-#     "Technical Skills": 0.1,
-#     "Education": 6,
-#     "Academic Background": 0.1,
-#     "Professional Experience": 4,
-#     "Work Experience": 4,
-#     "javascript": 0.1,
-#     "Employment History": 5,
-#     "Relevant Experience": 0.1,
-#     "Projects": 0.1,
-#     "Certifications": 0.1,
-#     "Awards and Honors": 3,
-#     "Achievements": 2,
-#     "Publications": 3,
-#     "Languages": 1,
-#     "Interests": 0.1,
-#     "Volunteer Work": 0.1,
-#     "References": 0.1,
-# }
-
-# good_nlp = []
-# bad_nlp = []
-# # chat gpt did this method only
-# def evaluate_resume(resume_text, database_evaluation):
-#     # Tokenize and preprocess the resume
-#     doc = nlp(resume_text.lower())
-
-#     # Calculate the score based on keyword presence and importance
-#     score = sum(keywords[key] for key in keywords if key in resume_text.lower())
-
-#     # Compare the score with the database evaluation
-#     if score > 3.0:  # Adjust the threshold as needed
-#         return "good"
-#     else:
-#         return "bad"
-
-# # Example usage:
-# resume_text = "This is an example resume with good experience and skills."
-# database_evaluation = "good"  # Replace with your actual database evaluation
-
-# for resume in dataset:
-#     result = evaluate_resume(resume_text, database_evaluation)
-#     if(result == "good") : good_nlp.append(resume)
-#     else : bad_nlp.append(resume)
-#     # print(f"Resume is {result}")
-
-
+    return vals
 
--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import PyPDF2
-import csv
+import base64
 from collections import Counter
 import os
 import pandas as pd
@@ -31,15 +31,36 @@ def main():
 
         parsed_text = parse_pdf_resume(file_path)
         result = assess_resume(parsed_text)
+        # words = get_most_used_words('UpdatedResumeDataSet.csv')
+        # missing = getMissingWords(parsed_text, words)
 
         st.header("Resume Analysis Results")
         st.write("Your Resume Score: ", result)
+        # st.write("Words that could maek your resume better: ", missing)
         st.write(
             "The resume score is calculated based on the presence of key terms and phrases "
             "found in a well-constructed resume. The model analyzes factors such as skills, "
             "experience, and education to determine the overall quality of the resume. "
             "A score above 25 is indicative of a strong resume."
         )
+
+        if result[0] == "Bad resume":
+            st.header("Improvement Suggestions")
+            st.write("Here are some samples of good resumes for ideas to improve:")
+
+            # Replace the image paths below with the actual paths of your images
+            image_paths = ["image1.png", "image2.png"]
+
+            # Display images in a horizontal row
+            for i, image_path in enumerate(image_paths, start=1):
+            # Add the "fullscreenable" class to each image
+                # st.image(image_path, caption=f"Example {i}", width=200)
+                st.write(f"Example{i}")
+
+                pdf_file_path = f"image{i}.pdf"  # Corrected line with f-string
+
+                pdf_embed = f'<iframe src="data:application/pdf;base64,{base64.b64encode(open(pdf_file_path, "rb").read()).decode()}" width="100%" height="600px"></iframe>'
+                st.markdown(pdf_embed, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
